ed1e545ab71c7c1f08592168d97da95148c4c5a3
Jose Gomez <angel.lg125@gmail.com>
2017-08-31 00:09:01 -0500

    Organize the html and Javascript files structure to make them easy to understand

--- Curso/Practica 2/scripts/my_scripts.js
+++ Curso/Practica 2/scripts/my_scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		$('.guess_box p').remove();
 		var discount = Math.floor((Math.random()*10 + 5));
 		var discount_msg = "<p>Ganaste un descuento del"+discount+" %</p>";
-		
+
 		$(this).append(discount_msg); // This me ayuda a saber sobre que elemento estoy dando click
 	});
 });
